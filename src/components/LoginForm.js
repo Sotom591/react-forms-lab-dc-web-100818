f34340d1d30513a94de199d10977a6e4e9027006
github username <github email address>
2018-12-12 17:53:00 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,38 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+handleLogin = (e) => {
+  this.setState({
+    [e.target.name]: e.target.value
+  })
+}
+
+handleSubmit = (e) => {
+  e.preventDefault()
+  let username = this.state.username
+  let password = this.state.password
+  username.length > 0 && password.length > 0 ? this.props.onSubmit({ username, password }) : null
+}
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleLogin}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleLogin}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,26 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
+
   constructor() {
     super();
+    this.state = {
 
-    this.state = {};
+      input: ""
+    };
   }
 
+handleChange = (e) => {
+  this.setState({
+    input: e.target.value
+  })
+}
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.input} onChange={e => this.handleChange(e)}/>
+        <h4>Remaining Chars: {(this.props.maxChars) - (this.state.input.length)}</h4>
       </div>
     );
   }
